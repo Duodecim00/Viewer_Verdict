0c7e085210616102069295ad1202239b04eb3bc4
Duodecim00 <josem2740@gmail.com>
2023-12-10 20:11:55 -0400

    contruccion de la pagina de catalogo y comunidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CommonModule],
